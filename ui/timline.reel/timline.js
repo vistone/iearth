ab07565d2ea3ea0e51d36a5ef7767d80eb09f5e3
stone <noreply>
2015-03-06 07:09:02 +0000

    Add component timline

---
+++ ui/timline.reel/timline.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/timline.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Timline
+ * @extends Component
+ */
+exports.Timline = Component.specialize(/** @lends Timline# */ {
+    constructor: {
+        value: function Timline() {
+            this.super();
+        }
+    }
+});
